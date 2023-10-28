259811ea3068d36461773f06cbfe14e4688c1dcc
tamdoan2809 <147482110+tamdoan2809@users.noreply.github.com>
2023-10-29 03:54:23 +0700

    Create DangKy.Designer.cs

---
+++ DoAn_1/DoAn_1/DangKy.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DoAn_1
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_dky = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài Khoản";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(164, 97);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(215, 27);
+            this.txt_user.TabIndex = 1;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(164, 169);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(215, 27);
+            this.txt_pass.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // btn_dky
+            // 
+            this.btn_dky.Location = new System.Drawing.Point(164, 239);
+            this.btn_dky.Name = "btn_dky";
+            this.btn_dky.Size = new System.Drawing.Size(100, 43);
+            this.btn_dky.TabIndex = 4;
+            this.btn_dky.Text = "Đăng Ký";
+            this.btn_dky.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(164, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Đăng Ký ";
+            // 
+            // DangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 335);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_dky);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label1);
+            this.Name = "DangKy";
+            this.Text = "DangKy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txt_user;
+        private TextBox txt_pass;
+        private Label label2;
+        private Button btn_dky;
+        private Label label3;
+    }
+}
